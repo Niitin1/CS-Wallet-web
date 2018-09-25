08033188c81df3321e059240b67b8401edcbb805
IGN <mcshe@DESKTOP-SQT517A>
2018-09-25 13:44:13 +0300

    Commit 268. Edits in obtaining transaction data and smartcontract data

--- src/main/java/com/credits/wallet/utils/WalletWebConverter.java
+++ src/main/java/com/credits/wallet/utils/WalletWebConverter.java
@@ -2,24 +2,48 @@ package com.credits.wallet.utils;
 
 import com.credits.common.exception.CreditsCommonException;
 import com.credits.common.utils.Converter;
+import com.credits.leveldb.client.data.SmartContractData;
+import com.credits.leveldb.client.data.SmartContractInvocationData;
 import com.credits.leveldb.client.data.TransactionFlowData;
+import com.credits.leveldb.client.data.VariantData;
 import com.credits.wallet.domain.Transaction;
 
-import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WalletWebConverter {
 
     public static TransactionFlowData transactionToTransactionFlowData(Transaction transaction) throws CreditsCommonException {
         TransactionFlowData transactionFlowData = new TransactionFlowData(
-                transaction.getInnerId(),
-                transaction.getSource(),
-                transaction.getTarget(),
+                transaction.getId(),
+                Converter.decodeFromBASE58(transaction.getSource()),
+                Converter.decodeFromBASE58(transaction.getTarget()),
                 transaction.getAmount(),
                 transaction.getBalance(),
                 transaction.getCurrency(),
-                ByteBuffer.wrap(Converter.decodeFromBASE58(transaction.getSignatureBase58())),
+                Converter.decodeFromBASE58(transaction.getSignatureBase58()),
                 transaction.getOfferedMaxFee()
         );
         return transactionFlowData;
     }
+
+    public static SmartContractInvocationData smartContractDataToSmartContractInvocationData(
+            SmartContractData smartContractData,
+            String methodName,
+            List<String> paramsVals,
+            boolean forgetNewState
+    ) {
+        // TODO Create VariantData list
+        List<VariantData> variantDataList = new ArrayList<>();
+        SmartContractInvocationData smartContractInvocationData = new SmartContractInvocationData(
+                smartContractData.getSourceCode(),
+                smartContractData.getByteCode(),
+                smartContractData.getHashState(),
+                methodName,
+                variantDataList,
+                forgetNewState
+        );
+        return smartContractInvocationData;
+    }
+
 }
